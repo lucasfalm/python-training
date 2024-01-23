a775049c3555b604a5ba80d8d1d5b370b275bd4c
Lucas Feijo <feijolucas1997@gmail.com>
2024-01-24 00:27:09 +1300

    :sparkles: Feat: strings and lists

---
+++ strings/examples.py
@@ -0,0 +1,25 @@
+#
+# NOTE: using str#format
+#
+some_string = "hey you, {}! {}".format("Lucas", "welcome to Python!")
+
+#
+# NOTE: possible to pass index positions
+#
+other_string = "that's {1}, isn't {0}?".format("it", "cool")
+
+#
+# NOTE: and aliases (and also optionally using 'f' instead of '.format' - new version)
+#
+i, c = "it", "cooler"
+another_string = f"I think this is even {c}, isn't {i}?"
+
+#
+# NOTE: formatting floats/precisions { value:width.precision f }
+#
+precision_float_string = "the result is {n:1.3f}".format(n=3.1209302)
+
+print(some_string)
+print(other_string)
+print(another_string)
+print(precision_float_string)
