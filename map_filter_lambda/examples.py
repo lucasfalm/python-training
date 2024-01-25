15c0c3e90b67c072478025cfabaaf2dafd693e02
Lucas Feijo <feijolucas1997@gmail.com>
2024-01-25 21:04:55 +1300

    :sparkles: Feat: map and filter functions + lambda

---
+++ map_filter_lambda/examples.py
@@ -0,0 +1,30 @@
+my_list = [1, 2, 3, 4]
+
+def is_odd(number):
+  return number % 2 != 0
+
+def is_even(number):
+  return number % 2 == 0
+
+#
+# NOTE: filter method
+#
+my_odd_list = list(filter(is_odd, my_list))
+
+my_even_list = list(filter(is_even, my_list))
+
+print(my_odd_list, my_even_list)
+
+#
+# NOTE: map method
+#
+my_new_list = list(map(lambda number: number * 2, my_list)) # using lambda (anonymous function)
+
+print(my_new_list)
+
+def format_number(number):
+  return f"#{str(number)}"
+
+my_other_new_list = list(map(format_number, my_list))
+
+print(my_other_new_list)
