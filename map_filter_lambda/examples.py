f4c23ca8ea676fd6a216a33def307d43fbcea8fd
Lucas Feijo <feijolucas1997@gmail.com>
2024-01-29 12:19:34 +1300

    :memo: Doc: comments

--- map_filter_lambda/examples.py
+++ map_filter_lambda/examples.py
@@ -6,21 +6,29 @@ def is_odd(number):
 def is_even(number):
   return number % 2 == 0
 
+# ------------ X ------------ X ------------ #
+
 #
 # NOTE: filter method
 #
+
 my_odd_list = list(filter(is_odd, my_list))
 
 my_even_list = list(filter(is_even, my_list))
 
 print(my_odd_list, my_even_list)
 
+# ------------ X ------------ X ------------ #
+
 #
 # NOTE: map method
 #
+
 my_new_list = list(map(lambda number: number * 2, my_list)) # using lambda (anonymous function)
 
 print(my_new_list)
+
+# ------------ X ------------ X ------------ #
 
 def format_number(number):
   return f"#{str(number)}"
