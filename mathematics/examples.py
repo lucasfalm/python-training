0dbb9f9a865bccf9b28f31563e2b49efd1f62c4a
Lucas Feijo <feijolucas1997@gmail.com>
2024-01-29 15:56:54 +1300

    :sparkles: Feat: math

---
+++ mathematics/examples.py
@@ -0,0 +1,10 @@
+import math
+
+some_float = 8.21
+print(math.floor(some_float)) # => 8 (rounded integer)
+print(math.ceil(some_float))  # => 9 (next integer above)
+print(math.log10(some_float)) # => 0.9143431571194408
+
+#
+# NOTE: numpy library has deeper math functions than the standard library
+#
