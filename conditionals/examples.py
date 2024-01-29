da4706121577a6604b6270bd8ac22e298698a6ee
Lucas Feijo <feijolucas1997@gmail.com>
2024-01-29 15:27:24 +1300

    :sparkles: Feat: collections module

--- conditionals/examples.py
+++ conditionals/examples.py
@@ -1,8 +1,10 @@
-ok = False
+# ------------ X ------------ X ------------ #
 
-if ok:
-  print("seems fine")
-elif not ok:
-  print("that's bad")
-else:
-  print("not sure")
+from collections import Counter
+
+my_list         = [1, 2, 3, 4, 5, 6, 6, 2, 1, 1, 1, 1, 1, 1]
+my_list_counter = Counter(my_list)
+
+print(my_list_counter) # => Counter({1: 7, 2: 2, 6: 2, 3: 1, 4: 1, 5: 1})
+
+# ------------ X ------------ X ------------ #
