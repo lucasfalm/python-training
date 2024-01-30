fdbc910a7b714dea695a3353c211ae0112a720d4
Lucas Feijo <feijolucas1997@gmail.com>
2024-01-31 00:54:57 +1300

    :memo: Doc: comments

--- sets/examples.py
+++ sets/examples.py
@@ -16,11 +16,21 @@ some_set.add("2")
 
 other_set = set(["1", "3"])
 
+some_set.union(other_set) # => join
+
+#
+# NOTE: returns the elements that are either in the first set
+#       or the second set, but not in both)
+#
 some_set.difference(other_set)
 
-some_set.union(other_set)
-
+#
+# NOTE: returns a new set containing elements that are in the
+#       first set but not in the second set
+#
 some_set.symmetric_difference(other_set)
+
+some_set.issubset(other_set) # => includes
 
 #
 # NOTE: cast list to set (make unique items)
